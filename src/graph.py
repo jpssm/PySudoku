441297a4e5b525e43c8dd6c381fdc30d7caa5b6d
Jhon <joaopedrotj@hotmail.com>
2022-04-19 08:07:16 +0800

    Atualização da classe Graph

--- src/graph.py
+++ src/graph.py
@@ -7,8 +7,9 @@ class Vertex:
         self.label = None #Rotulo do vértice
     
     def addNeighbor(self,vertex) -> None: #Adiciona um vértice a lista de vizinhos
-        self.adjacentList.append(vertex)
-    
+        if not(vertex in self.adjacentList): #Adiciona o vértice somente se ele já não estiver na lista
+            self.adjacentList.append(vertex)
+
     def getNeighborhood(self) -> list: #Retorna a lista com a vizinhança de vértices adjacentes
         return(self.adjacentList)
 
@@ -18,15 +19,14 @@ class Vertex:
     def getLabel(self) -> int: #Retorna o rótulo do vértice
         return(self.label)
     
+    
+    
 
 class Graph:
     def __init__(self) -> None:
         self.vertexlist = {} #A lista de vértices é implementado através de um dicionário
     
-    def addVextex(self,name) -> None: #Adiciona ao grafo um vértice com o nome dado
-        self.vertexlist[name] = Vertex(name)
-    
-    def addEdge(self,nameVertex1,nameVertex2) -> None: #Adiciona uma aresta entre os vértices
+    def addEdge(self, nameVertex1, nameVertex2) -> None: #Adiciona uma aresta entre os vértices
         self.vertexlist[nameVertex1].addNeighbor(self.vertexlist[nameVertex2]) #Adiciona o vértice 1 a lista de vizinhos do 2
         self.vertexlist[nameVertex2].addNeighbor(self.vertexlist[nameVertex1]) #Adiciona o vértice 2 a lista de vizinhos do 1
     
